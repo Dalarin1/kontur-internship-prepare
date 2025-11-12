15438d1439efa97715fc67fe430abd2adc324457
Dalarin1 <iluchin_vlados@mail.ru>
2025-11-12 15:33:31 +0500

    init

--- compiler.py
+++ compiler.py
@@ -3,14 +3,15 @@ import os
 
 TEST_DIR = os.getcwd() + "/test"
 years_folders = [i for i in os.listdir(TEST_DIR) if i != "include"]
-years_folders.remove("summer-2022")
+years_folders.remove("summer-2022") # Там сишарп
 
 for year in years_folders:
     fullpath = TEST_DIR + "/" + year
 
     for file in os.listdir(fullpath):
         print(fullpath + "/" + file)
-
+        
+        
         cmd = f"g++ -o {fullpath + '/' + file.replace('.cpp', '.exe')} -I {TEST_DIR}/include -O3 -s {fullpath + '/' + file}"
         if os.system(cmd) == 0:
             print(f"✓ Успешно: {file}")
